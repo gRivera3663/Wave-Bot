2a5111b6f97ef0ad73ec556fd28d5d552431298d
Gabriel Rivera <gabrielrivera457@gmail.com>
2020-10-07 20:20:24 -0700

    added $jack and fixed $message bug

--- events/message.js
+++ events/message.js
@@ -1,10 +1,67 @@
+// prefix for commands
 const prefix = "$"
+
+// Library requirements
 const { MessageAttachment } = require("discord.js")
 const ytdl = require("ytdl-core")
+
+// Steam Options for YTDL
 const streamOptions = {
     format: "audioonly",
     quality: "highestaudio"
 }
+
+// Links to Jack in the Box Commercials for $jack
+const jackLinks = [
+    "https://www.youtube.com/watch?v=AJF9NUQSRIM",
+    "https://www.youtube.com/watch?v=v1v-16QUESk",
+    "https://www.youtube.com/watch?v=BYMt_HdQmns",
+    "https://www.youtube.com/watch?v=m2TApGQUWF0",
+    "https://www.youtube.com/watch?v=uKvD7R7wDO8",
+    "https://www.youtube.com/watch?v=bN3j_i847b4",
+    "https://www.youtube.com/watch?v=OZFA87ZF71U",
+    "https://www.youtube.com/watch?v=W1GtVPDjfwc",
+    "https://www.youtube.com/watch?v=zf5i0O2PNS4",
+    "https://www.youtube.com/watch?v=QdKwWUIo2r4",
+    "https://www.youtube.com/watch?v=PyusO5s8YIA",
+    "https://www.youtube.com/watch?v=tFZpazWqOFw",
+    "https://www.youtube.com/watch?v=8OsjlRgakkM",
+    "https://www.youtube.com/watch?v=o5MXLep0cOo",
+    "https://www.youtube.com/watch?v=1aCh_3boZ7g",
+    "https://www.youtube.com/watch?v=sRv9_Q3HOmg",
+    "https://www.youtube.com/watch?v=fkwOdaFRHlw",
+    "https://www.youtube.com/watch?v=xS2O_Dxkldw",
+    "https://www.youtube.com/watch?v=RkG7DF1w5GI",
+    "https://www.youtube.com/watch?v=r5rlbOqQq7w",
+    "https://www.youtube.com/watch?v=7lsvFX1AWTc",
+    "https://www.youtube.com/watch?v=pD5-Ov1pwL8",
+    "https://www.youtube.com/watch?v=PmUgEdf9UdI",
+    "https://www.youtube.com/watch?v=XVjO4N-7ZwQ",
+    "https://www.youtube.com/watch?v=2TkRn2AfNCc",
+    "https://www.youtube.com/watch?v=1AX6-1JdDsA",
+    "https://www.youtube.com/watch?v=8IFpt6lkiwA",
+    "lhttps://www.youtube.com/watch?v=zzNFYFAS9ZA",
+    "https://www.youtube.com/watch?v=091fePpOojM",
+    "https://youtu.be/2JCDcSXDugo",
+    "https://www.youtube.com/watch?v=f6Cls-4t9B8",
+    "https://youtu.be/IQSVfrDb0pM",
+    "https://youtu.be/JWA1AFqzYnU",
+    "https://youtu.be/Do5Ctnw0b3g",
+    "https://youtu.be/JQLZL7x23Zo",
+    "https://youtu.be/9dCGdNeBzLg",
+    "https://youtu.be/OC8Fhhlj2eU",
+    "https://youtu.be/RbOkOwsqBuo",
+    "https://youtu.be/rOWk4-hsFFM",
+    "https://youtu.be/M-flk-U8Kj0",
+    "https://youtu.be/ek20iEplvgE",
+    "https://www.youtube.com/watch?v=5L8hHDcFDGY",
+    "https://youtu.be/pKdnCQzf_uQ",
+    "https://youtu.be/ShxsfnNcknw",
+    "https://www.youtube.com/watch?v=Yns0DtN97UQ",
+    "https://www.youtube.com/watch?v=Ay-RsLbGVmk",
+    "https://www.youtube.com/watch?v=1izHRMqgqJs",
+    "https://www.youtube.com/watch?v=xk7iJr3pZ2E"
+]
 
 // Used to play the given YouTube link in the message sender's voice channel
 function playLink(message, link){
@@ -79,37 +136,53 @@ module.exports = (client, message) => {
                 })
             }
         }
+        // jack
+        else if (command == "jack"){
+            var index = Math.floor(Math.random() * jackLinks.length)
+            message.channel.send(jackLinks[index])
+        }
         // ERROR FOR share
         else if (command == "share" && isUserOnSpotify(user) === 0){
             message.reply("to use this command your Spotify must be connected to your Discord and the \"Display Spotify as your status\" setting must be turned on. You can enable this in Settings -> Connections")
         }  
         // help
         else if (command == "help"){
-            var text = "```Wave Bot Commands\n***NOTE: FOR ANY ADMIN HELP PLEASE DM THE BOT***\n-------------------------\nVOICE CHANNEL SOUND COMMANDS\n(Sounds that play while in a voice channel.)\n[Must be in a voice channel for these sounds to play.]\n\n$driveup\n$opu\n-------------------------\nTEXT CHANNEL RESPONSE COMMANDS\n(Bot just responds in text channel.)\n\n$redcard\n$god\n-------------------------\nCOMMANDS\n(Each command does something different. Anything in \"<>\"s are other things the command needs to work.)\n\n$share [Shares the current song you are listening to on Spotify. Spotify must be connected and visible as your status for this to work.]\n-------------------------\```"
+            var text = "```Wave Bot Commands\n" +
+            "***NOTE: FOR ANY ADMIN HELP PLEASE DM THE BOT***\n" +
+            "-------------------------\n" +
+            "VOICE CHANNEL SOUND COMMANDS\n\n" +
+            "$driveup\n" +
+            "$opu\n" +
+            "-------------------------\n" +
+            "COMMANDS\n" +
+            "(Anything in \"<>\"s are other things the command needs to work.)\n\n" +
+            "$redcard\n" +
+            "$god\n" +
+            "$jack\n" +
+            "$share >> Shares the current song you are listening to on Spotify.\n" +
+            "-------------------------```"
+
             var channel = message.channel
             channel.send(text)
 
             // Raw Text
             /*
-
             Wave Bot Commands
             ***NOTE: FOR ANY ADMIN HELP PLEASE DM THE BOT***
             -------------------------
             VOICE CHANNEL SOUND COMMANDS
-            (Sounds that play while in a voice channel.)
-            [Must be in a voice channel for these sounds to play.]
+
             $driveup
             $opu
-            \n
-            TEXT CHANNEL RESPONSE COMMANDS
-            (Bot just responds in text channel.)
+            -------------------------
+            OTHER COMMANDS
+            (Anything in "<>"s are other things the command needs to work.)
+
             $redcard
             $god
-            \n
-            COMMANDS
-            (Each command does something different. Anything in "<>"s are other things the command needs to work.)
-            $share [Shares the current song you are listening to on Spotify. Spotify must be connected and visible as your status for this to work.]
-
+            $jack
+            $share >> Shares the current song you are listening to on Spotify.
+            -------------------------
             */
         }
     }
@@ -147,7 +220,12 @@ module.exports = (client, message) => {
             }
             // controls
             else if (command == "controls"){
-                var text = "```Admin Wave Bot Commands\n(Please note that \"$controls\" is for admin controls, while \"$help\" is for public commands)\n-------------------------\nMESSAGES THE USER VIA THE WAVE BOT\n$message <@user> <message content>```"
+                var text = "```Admin Wave Bot Commands\n" +
+                "(Please note that \"$controls\" is for admin controls, while \"$help\" is for public commands)\n" +
+                "-------------------------\n" +
+                "$message <@user> <message content> >> Messages the given user with the Wave Bot.\n" +
+                "-------------------------```"
+
                 var channel = client.channels.fetch(process.env.ADMIN_CHANNEL)
                 channel.then(message => {
                     message.send(text).catch(error => {console.log(error)})
@@ -158,8 +236,8 @@ module.exports = (client, message) => {
                 Admin Wave Bot Commands
                 (Please note that "$controls" is for admin help, while "$help" is for public commands)
                 -------------------------
-                MESSAGES THE USER VIA THE WAVE BOT
-                $message <@user> <message content>
+                $message <@user> <message content> >> Messages the given user with the Wave Bot.
+                -------------------------
                 */
             }
         }
@@ -171,7 +249,7 @@ module.exports = (client, message) => {
         var msg = message.content.toString()
         var user = message.author.id
 
-        if (user != "Wave Bot#8159"){
+        if (user != process.env.BOT_ID){
             channel.then( message => {
                 var temp = "<@" + user + "> sent a message to the Wave Bot: " + msg
                 message.send(temp).catch(error => {console.log(error)})
